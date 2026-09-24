c6f0e199a5060126f013e57ba7c8017eb2434877
Princee Pandey <115348458+prinshu404@users.noreply.github.com>
2026-09-24 20:53:04 +0530

    Auto update

---
+++ backend/api/resumeApi.js
@@ -0,0 +1,12 @@
+import api from "./api";
+
+const resumeApi = {
+  analyzeResume: (formData) =>
+    api.post("/resume/analyze", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }),
+};
+
+export default resumeApi;
